52d882bea40d672b67b2a3bd2421b585b0f86d10
huxiangli <1366287517@qq.com>
2018-01-18 20:08:53 +0800

    托运单信息管理模块增加导出功能

--- src/distributionCarManage/pages/consignInfoManage.vue
+++ src/distributionCarManage/pages/consignInfoManage.vue
@@ -24,7 +24,10 @@
 				  </el-select>
 				</el-form-item>
 				<el-form-item>
-					<el-button type="primary" size="small" @click.native="getData">查询</el-button>
+					<el-button type="primary" size="small" @click.native="searchData">查询</el-button>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" size="small" @click.native="exportExcel">导出</el-button>
 				</el-form-item>
 			</el-form>
 		</div>
@@ -37,14 +40,16 @@
 		        <el-input v-model="scope.row.Consign_No" placeholder="请输入托运单号" @blur="save(scope.row)" :class="{'saveSuccess': scope.row.saveSuccess && (scope.row.Consign_No.trim() !== '')}" @focus="scope.row.saveSuccess = false"></el-input>
 		      </template>
 	      </el-table-column>
-	      <el-table-column prop="Carrier_Name" label="承运商" :show-overflow-tooltip="true"></el-table-column>
+	      <el-table-column prop="Carrier_Name" label="承运商" :show-overflow-tooltip="true" width="200"></el-table-column>
 	      <el-table-column prop="Biz_Bill_No" label="业务单据编号" width="140"></el-table-column>
-	      <el-table-column prop="Con_Name" label="委托方" :show-overflow-tooltip="true"></el-table-column>
+	      <el-table-column prop="Con_Name" label="委托方" :show-overflow-tooltip="true" width="200"></el-table-column>
 	      <el-table-column prop="Ssa_Name" label="单位名称" width="180" :show-overflow-tooltip="true"></el-table-column>
-	      <el-table-column prop="Contact_Name" label="联系人" width="80"></el-table-column>
-	      <el-table-column prop="Contact_Phone" label="联系电话" width="130" :show-overflow-tooltip="true"></el-table-column>
-	      <el-table-column prop="Address" label="联系地址" :show-overflow-tooltip="true"></el-table-column>
-	      <el-table-column prop="Actual_Gross_Pcs" label="件数" width="80"></el-table-column>
+	      <el-table-column prop="Contact_Name" label="联系人" width="120" :show-overflow-tooltip="true"></el-table-column>
+	      <el-table-column prop="Contact_Phone" label="联系电话" width="140" :show-overflow-tooltip="true"></el-table-column>
+	      <el-table-column prop="Address" label="联系地址" :show-overflow-tooltip="true" width="200"></el-table-column>
+	      <el-table-column prop="TotalPcs_Num" label="件数" width="80"></el-table-column>
+	      <el-table-column prop="Gross_Volume" label="体积(m³)" width="100"></el-table-column>
+	      <el-table-column prop="Gross_Weight" label="重量(KG)" width="100"></el-table-column>
 	    </el-table>
 		</div>
 		<div class="block">
@@ -64,7 +69,8 @@
 <script>
 	import Vue from 'vue'
 	import Api from '@/common/js/api'
-	import {DateFtt} from '@/common/js/utils'
+	import {DateFtt, formatJson} from '@/common/js/utils'
+	import {json2excel} from '@/common/js/excel.js'
 	import consiModal from '@/common/service/modal/consi-modal'
 	import commonModal from '@/common/components/common-modal'
 	export default {
@@ -92,6 +98,7 @@
 					}
 				],
 				list: [],
+				exportList: [],
 				currentPage: 1,
 				pageSize: 10,
 				bigTotalItems: 0,
@@ -122,6 +129,10 @@
 					currentPage: 1,
 					pageSize: 10,
 					bigTotalItems: 0
+				},
+				tableOptions: {
+					titles: ['发货日期', '托运单号', '承运商', '业务单据编号', '委托方', '单位名称', '联系人', '联系电话', '联系地址', '件数', '体积(m³)', '重量(KG)'],
+					fields: ['Invoice_Time', 'Consign_No', 'Carrier_Name', 'Biz_Bill_No', 'Con_Name', 'Ssa_Name', 'Contact_Name', 'Contact_Phone', 'Address', 'TotalPcs_Num', 'Gross_Volume', 'Gross_Weight']
 				}
 			}
 		},
@@ -195,6 +206,29 @@
 					this.loadingWait.close()
 				})
 			},
+			getExportData () {
+				let startTime = this.filterCondition.rangeDate[0]
+				let endTime = this.filterCondition.rangeDate[1]
+				let params = {
+					Start_Date: (typeof (startTime) === 'string') ? startTime : DateFtt('yyyy-MM-dd', startTime),
+					End_Date: (typeof (endTime) === 'string') ? endTime : DateFtt('yyyy-MM-dd', endTime),
+					Operator_Id: Api.userInfo.Operator_Id,
+					Con_Id: this.filterCondition.Con_Id,
+					Carrier_Id: this.filterCondition.Carrier_Id,
+					Ssa_Name: '%' + ((this.filterCondition.Ssa_Name === '') ? '%' : this.filterCondition.Ssa_Name) + '%',
+					Biz_Bill_No: '%' + ((this.filterCondition.Biz_Bill_No === '') ? '%' : this.filterCondition.Biz_Bill_No) + '%',
+					State: (this.filterCondition.State === '') ? '%' : this.filterCondition.State
+				}
+				Api.get('TMP_TransportEntrust_Consign_Export', params, true).then((res) => {
+					if (res.Flag) {
+						this.exportList = res.MsgInfo
+					}
+				})
+			},
+			searchData () {
+				this.getData()
+				this.getExportData()
+			},
 			getCarrier () {
 				let params = {
 					Keywords: '%' + this.Carrier.Keywords + '%',
@@ -235,13 +269,24 @@
 					obj.saveSuccess = true
 				})
 			},
+			exportExcel () {
+				if (this.exportList.length === 0) {
+          this.$alert('当前无数据，无法导出', '提示', {
+          	confirmButtonText: '确定'
+          })
+          return false
+        }
+        let jsonData = formatJson(this.exportList, this.tableOptions.fields)
+        json2excel(this.tableOptions.titles, jsonData)
+			},
 			init () {
-				let startTime = DateFtt('yyyy-MM-dd', new Date()).substring(0, 8) + '01'
+				let startTime = DateFtt('yyyy-MM-dd', new Date()).substring(0, 8) + (new Date().getDate() - 5)
 				let endTime = DateFtt('yyyy-MM-dd', new Date())
 				this.filterCondition.rangeDate.push(startTime)
 				this.filterCondition.rangeDate.push(endTime)
 				this.getData()
 				this.getCarrier()
+				this.getExportData()
 			}
 		},
 		mounted () {
