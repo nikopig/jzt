8adf6a1e406baf7c8b8266430904293c247674e2
huxiangli <1366287517@qq.com>
2018-02-26 16:28:24 +0800

    修改element-ui主题皮肤名称

--- src/infoManage/main.js
+++ src/infoManage/main.js
@@ -33,7 +33,7 @@ import {
   inputNumber
 } from 'element-ui'
 import 'normalize.css'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 import '@/common/style/icon.css'
 
 Vue.use(Checkbox)
