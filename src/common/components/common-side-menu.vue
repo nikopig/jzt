f8680a02021648488c1d91c67e1fa64a18641aba
xiaoming <834557263@qq.com>
2018-02-27 09:27:09 +0800

    增加-公共组件-侧边栏菜单组件

---
+++ src/common/components/common-side-menu.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'commonSideMenu',
+        props: [],
+        components: {},
+        data () {
+            return {
+              menus: [
+                {
+                  name: '数据接口服务',
+                  path:'/a',
+                  icon: '',
+                  level: 1,
+                  children: [
+                    {
+                      name: '分菜单1',
+                      path:'/aa',
+                      icon: '',
+                      level: 2
+                    },
+                    {
+                      name: '分菜单2',
+                      path:'/ab',
+                      icon: '',
+                      level: 2
+                    },
+                    {
+                      name: '分菜单3',
+                      path:'/ac',
+                      icon: '',
+                      level: 2
+                    }
+                  ]
+                }
+              ]
+            }
+        },
+        computed: {},
+        methods: {},
+        created () {
+        },
+        mounted () {
+        }
+    }
+</script>
+<style lang="less" scoped>
+</style>
