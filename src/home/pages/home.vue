736fc0f817f9864a062913a125fbc250e35c0674
xiaoming <834557263@qq.com>
2018-02-27 11:00:57 +0800

    合并迁移

---
+++ src/home/pages/home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="common-container">
+      <common-side-menu></common-side-menu>
+    </div>
+</template>
+
+<script>
+    import commonSideMenu from '@/common/components/common-side-menu'
+    export default {
+        name: 'home',
+        props: [],
+        components: {commonSideMenu},
+        data () {
+            return {}
+        },
+        computed: {},
+        methods: {},
+        created () {
+        },
+        mounted () {
+        }
+    }
+</script>
+<style lang="less" scoped>
+</style>
